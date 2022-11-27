4ef3395cc13297f18410ad46077beff7bfe622fc
Zaldef <guilhermeroelli@gmail.com>
2022-11-27 18:15:53 -0300

    0.22
    Ligeiras correções de css e logica de resgate de informações com o banco

--- resources/views/cursos/curso.blade.php
+++ resources/views/cursos/curso.blade.php
@@ -12,11 +12,11 @@
       <div id="info-container" class="col-md-6">
         <h1>{{ $curso->name }}</h1>
        <p class="curso-simplified_description"><ion-icon name="reader-outline"></ion-icon> {{ $curso->simplified_description}}</p>
-       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Minimos: $curso->alunosqtdM</p>
-       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Matriculados: $curso->alunosqtdM / {{ $curso->alunosqtd}}</p>
-       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Máximos: {{ $curso->alunosqtd}}</p>
-       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Status: Matrículas Abertas/Mínimo de alunos não atingido!/Matrículas Abertas - Curso acontecerá!/Matrículas Encerradas</p>
-       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Professor: </p>  
+       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Minimos: {{ $curso->alunosqtdmin}}</p>
+       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Matriculados: $curso_id->aluno_id</p>
+       <p class="curso-alunosqtd"><ion-icon name="people-outline"></ion-icon> Alunos Máximos: {{ $curso->alunosqtdmax}}</p>
+       <p class="curso-alunosqtd"><ion-icon name="file-tray-full-outline"></ion-icon> Status: Matrículas Abertas/Mínimo de alunos não atingido!/Matrículas Abertas - Curso acontecerá!/Matrículas Encerradas</p>
+       <p class="curso-alunosqtd"><ion-icon name="person-outline"></ion-icon> Professor: $professor->curso_id </p>  
         <a href="#" class="btn btn-primary" id="curso-submit">Encerrar Matriculas</a>
       </div>
       <div class="col-md-12" id="description-container">
